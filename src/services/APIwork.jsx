823b6a74afc4d70df22700be2db967cda9ffedda
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-08-15 21:31:14 +0300

    api

--- src/services/APIwork.jsx
+++ src/services/APIwork.jsx
@@ -1,5 +1,3 @@
-import { Component } from 'react';
-
 function getImages(foundImg, page) {
   return fetch(
     `https://pixabay.com/api/?key=28164685-e508b46b7d4362311384dafbb&q=${foundImg}&image_type=photo&pretty=true&orientation=horizontal&per_page=12&page=${page}`
